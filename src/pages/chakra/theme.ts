d34c7e91d602e46525297b11743b4b72dea356ea
sagar-pardhi <sgrpardhi007@gmail.com>
2023-01-31 12:31:44 +0530

    theme setup

---
+++ src/pages/chakra/theme.ts
@@ -0,0 +1,23 @@
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/700.css";
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#ff3c00",
+    },
+  },
+  fonts: {
+    body: "Open sans, sans-serif",
+  },
+  styles: {
+    global: () => ({
+      body: {
+        bg: "gray.200",
+      },
+    }),
+  },
+  components: {},
+});
