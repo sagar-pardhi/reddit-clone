f40b48a171d44bf0f2962b9c1c51a3948b0bf154
sagar-pardhi <sgrpardhi007@gmail.com>
2023-02-01 20:36:42 +0530

    added search input component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,23 @@
-import React from "react";
+import { Flex, Image } from "@chakra-ui/react";
+import SearchInput from "./SearchInput";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <Flex bg="white" h="44px" p="6px 12px">
+      <Flex align="center">
+        <Image src="/images/redditFace.svg" h="30px" />
+        <Image
+          src="/images/redditText.svg"
+          h="46px"
+          display={{
+            base: "none",
+            md: "unset",
+          }}
+        />
+      </Flex>
+      {/* <Directory /> */}
+      <SearchInput />
+      {/* <RightContent /> */}
+    </Flex>
+  );
 }
---
+++ src/components/Navbar/SearchInput.tsx
@@ -0,0 +1,34 @@
+import { SearchIcon } from "@chakra-ui/icons";
+import { Flex, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+
+type Props = {};
+
+export default function SearchInput({}: Props) {
+  return (
+    <Flex flexGrow={1} mr={2} align="center">
+      <InputGroup>
+        <InputLeftElement
+          pointerEvents="none"
+          children={<SearchIcon color="gray.400" mb={1} />}
+        />
+        <Input
+          placeholder="Search Reddit"
+          fontSize="10pt"
+          _placeholder={{ color: "gray.500" }}
+          _hover={{
+            bg: "white",
+            border: "1px solid",
+            borderColor: "blue.500",
+          }}
+          _focus={{
+            outline: "none",
+            border: "1px solid",
+            borderColor: "blue.500",
+          }}
+          h="34px"
+          bg="gray.50"
+        />
+      </InputGroup>
+    </Flex>
+  );
+}
