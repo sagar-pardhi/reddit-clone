dc9b88351da45820fff21f668308c24ce7f6dc82
sagar-pardhi <sgrpardhi007@gmail.com>
2023-02-04 20:48:23 +0530

    added navbar icons

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,19 +1,20 @@
 import AuthModel from "@/components/Modal/Auth/AuthModel";
 import { auth } from "@/firebase/clientApp";
 import { Button, Flex } from "@chakra-ui/react";
-import { signOut } from "firebase/auth";
+import { signOut, User } from "firebase/auth";
 import AuthButtons from "./AuthButtons";
+import Icons from "./Icons";
 
 type RightContentProps = {
-  user: any
+  user?: User | null;
 };
 
-export default function RightContent({user}: RightContentProps) {
+export default function RightContent({ user }: RightContentProps) {
   return (
     <>
       <AuthModel />
       <Flex justify="center" align="center">
-        {user ? <Button onClick={() => signOut(auth)}>Logout</Button> : <AuthButtons />}
+        {user ? <Icons /> : <AuthButtons />}
       </Flex>
     </>
   );
