21232f34201ff7172f4e675a21557bfec85539b1
sagar-pardhi <sgrpardhi007@gmail.com>
2023-02-13 20:30:16 +0530

    created new post form component and tab item component

---
+++ src/components/Posts/NewPostForm.tsx
@@ -0,0 +1,69 @@
+import { Flex, Icon } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { BiPoll } from "react-icons/bi";
+import { BsLink45Deg, BsMic } from "react-icons/bs";
+import { IoDocumentText, IoImageOutline } from "react-icons/io5";
+import { AiFillCloseCircle } from "react-icons/ai";
+import TabItem from "./TabItem";
+
+type NewPostFormProps = {};
+
+const formTabs: TabItemType[] = [
+  {
+    title: "Post",
+    icon: IoDocumentText,
+  },
+  {
+    title: "Images & Video",
+    icon: IoImageOutline,
+  },
+  {
+    title: "Link",
+    icon: BsLink45Deg,
+  },
+  {
+    title: "Poll",
+    icon: BiPoll,
+  },
+  {
+    title: "Talk",
+    icon: BsMic,
+  },
+];
+
+export type TabItemType = {
+  title: string;
+  icon: typeof Icon.arguments;
+};
+
+const NewPostForm: React.FC<NewPostFormProps> = () => {
+  const [selectedTab, setSelectedTab] = useState(formTabs[0].title);
+  const [textInput, setTextInput] = useState({
+    title: "",
+    body: "",
+  });
+  const [selectedFile, setSelectedFile] = useState<string>("");
+
+  const handleCreatePost = async () => {};
+
+  const onSelectImage = () => {};
+
+  const onTextChange = () => {};
+
+  return (
+    <Flex dir="column" bg="white" borderRadius={4} mt={2}>
+      <Flex w="100%">
+        {formTabs.map((item) => (
+          <TabItem
+            key={item.title}
+            item={item}
+            selected={item.title === selectedTab}
+            setSelectedTab={setSelectedTab}
+          />
+        ))}
+      </Flex>
+    </Flex>
+  );
+};
+
+export default NewPostForm;
--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,7 +1,20 @@
+import PageContent from "@/components/Layout/PageContent";
+import NewPostForm from "@/components/Posts/NewPostForm";
+import { Box, Text } from "@chakra-ui/react";
 import React from "react";
 
 const SubmitPostPage: React.FC = () => {
-  return <div>Submit</div>;
+  return (
+    <PageContent>
+      <>
+        <Box p="14px 0px" borderBottom="1px solid" borderColor="white">
+          <Text>Create a Post</Text>
+        </Box>
+        <NewPostForm />
+      </>
+      <>{/* About Component */}</>
+    </PageContent>
+  );
 };
 
 export default SubmitPostPage;
