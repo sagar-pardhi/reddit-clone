01a4e7bcb6f0646a908166fc3801f3cdf323349c
sagar-pardhi <sgrpardhi007@gmail.com>
2023-02-22 19:56:26 +0530

    added single post link sharing feature

--- src/pages/r/[communityId]/comments/[pid].tsx
+++ src/pages/r/[communityId]/comments/[pid].tsx
@@ -3,12 +3,37 @@ import PostItem from "@/components/Posts/PostItem";
 import usePosts from "@/hooks/usePosts";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "@/firebase/clientApp";
+import { auth, firestore } from "@/firebase/clientApp";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { doc, getDoc } from "firebase/firestore";
+import { Post } from "@/atoms/postAtom";
 
 const PostPage: React.FC = () => {
   const [user] = useAuthState(auth);
   const { postStateValue, setPostStateValue, onDeletePost, onVote } =
     usePosts();
+  const router = useRouter();
+
+  const fetchPost = async (postId: string) => {
+    try {
+      const postDocRef = doc(firestore, "posts", postId);
+      const postDoc = await getDoc(postDocRef);
+      setPostStateValue((prev) => ({
+        ...prev,
+        selectedPost: { id: postDoc.id, ...(postDoc.data() as Post) },
+      }));
+    } catch (error: any) {
+      console.log("fetchPost error", error);
+    }
+  };
+  useEffect(() => {
+    const { pid } = router.query;
+
+    if (pid && !postStateValue.selectedPost) {
+      fetchPost(pid as string);
+    }
+  }, [router.query, postStateValue]);
 
   return (
     <PageContent>
