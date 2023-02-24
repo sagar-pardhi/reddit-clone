91f4da6f5c1eb981d3bddbe6a61cb97e8e15a781
sagar-pardhi <sgrpardhi007@gmail.com>
2023-02-24 20:46:48 +0530

    added community navigation dropdown feature

--- src/components/Navbar/Directory/Communities.tsx
+++ src/components/Navbar/Directory/Communities.tsx
@@ -1,29 +1,70 @@
 import { useState } from "react";
 import CreateCommunityModal from "@/components/Modal/CreateCommunity/CreateCommunityModal";
-import { Flex, MenuItem, Icon } from "@chakra-ui/react";
+import { Flex, MenuItem, Icon, Text, Box } from "@chakra-ui/react";
 import { GrAdd } from "react-icons/gr";
+import { useRecoilValue } from "recoil";
+import { communityState } from "@/atoms/communitiesAtom";
+import MenuListItem from "./MenuListItem";
+import { FaReddit } from "react-icons/fa";
 
 type Props = {};
 
-export default function Communities({}: Props) {
+const Communities: React.FC<Props> = () => {
   const [open, setOpen] = useState(false);
+  const mySnippets = useRecoilValue(communityState).mySnippets;
 
   return (
     <>
       <CreateCommunityModal open={open} handleClose={() => setOpen(false)} />
-      <MenuItem
-        w="100%"
-        fontSize="10pt"
-        _hover={{
-          bg: "gray.100",
-        }}
-        onClick={() => setOpen(true)}
-      >
-        <Flex align="center">
-          <Icon fontSize={20} mr={2} as={GrAdd} />
-          Create Community
-        </Flex>
-      </MenuItem>
+
+      <Box mt={2} mb={4}>
+        <Text pl={3} mb={1} fontSize="7pt" fontWeight={500} color="gray.500">
+          Moderating
+        </Text>
+        {mySnippets
+          .filter((snippet) => snippet.isModerator)
+          .map((snippet) => (
+            <MenuListItem
+              key={snippet.communityId}
+              icon={FaReddit}
+              dsiplayText={`r/${snippet.communityId}`}
+              link={`/r/${snippet.communityId}`}
+              iconColor="brand.100"
+              imageURL={snippet.imageURL}
+            />
+          ))}
+      </Box>
+
+      <Box mt={2} mb={4}>
+        <Text pl={3} mb={1} fontSize="7pt" fontWeight={500} color="gray.500">
+          My Communities
+        </Text>
+        <MenuItem
+          w="100%"
+          fontSize="10pt"
+          _hover={{
+            bg: "gray.100",
+          }}
+          onClick={() => setOpen(true)}
+        >
+          <Flex align="center">
+            <Icon fontSize={20} mr={2} as={GrAdd} />
+            Create Community
+          </Flex>
+        </MenuItem>
+        {mySnippets.map((snippet) => (
+          <MenuListItem
+            key={snippet.communityId}
+            icon={FaReddit}
+            dsiplayText={`r/${snippet.communityId}`}
+            link={`/r/${snippet.communityId}`}
+            iconColor="blue.500"
+            imageURL={snippet.imageURL}
+          />
+        ))}
+      </Box>
     </>
   );
-}
+};
+
+export default Communities;
--- src/hooks/useCommunityData.tsx
+++ src/hooks/useCommunityData.tsx
@@ -69,6 +69,7 @@ const useCommunityData = () => {
       const newSnippet: CommunitySnippet = {
         communityId: communityData.id,
         imageURL: communityData.imageURL || "",
+        isModerator: user?.uid === communityData.creatorId,
       };
 
       batch.set(
